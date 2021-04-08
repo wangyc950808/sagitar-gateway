87f735b7e36ab3873da9f177c05f39ce196b2229
Administrator <870381736@qq.com>
2021-04-08 18:02:58 +0800

    docs:项目初始化

---
+++ src/main/java/cn/org/wangyc/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.org.wangyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wangyc
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
